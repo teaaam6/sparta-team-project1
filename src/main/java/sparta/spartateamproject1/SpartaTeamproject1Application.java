6bbdb691599345974c1b3e0460ad01e420d7e50d
Seungyoung <leesy960909@gmail.com>
2026-01-13 20:15:18 +0900

    first commit

---
+++ src/main/java/sparta/spartateamproject1/SpartaTeamproject1Application.java
@@ -0,0 +1,13 @@
+package sparta.spartateamproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaTeamproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaTeamproject1Application.class, args);
+    }
+
+}

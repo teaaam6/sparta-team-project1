183968f11a9f331e185f30036452888fe78bb63e
imprity <imprity041@gmail.com>
2026-01-15 15:35:25 +0900

    feat: item dto에 bean validation 추가

--- src/main/java/sparta/spartateamproject1/dto/ItemAddDto.java
+++ src/main/java/sparta/spartateamproject1/dto/ItemAddDto.java
@@ -10,16 +10,27 @@ import sparta.spartateamproject1.type.ItemStatus;
 
 import java.time.LocalDateTime;
 
+// TODO : 잘못된 enum의 경우 spring은 
+// org.springframework.http.converter.HttpMessageNotReadableException: JSON parse error: Cannot deserialize value of type
+//
+// 던집니다.
+//
+// 아직 이를 처리하는 exception handler가 없기 때문에
+// 서버는 단순히 badRequest를 던집니다. 물론 틀린건 아니지만 저희 처리를 할 수 있으면 좋을거 같습니다.
+
 public class ItemAddDto {
     @Getter
     @AllArgsConstructor
     public static class Request {
+        @NotBlank(message = "상품 이름은 비어있으면 않됩니다")
         private String name;
 
         private ItemCategory category;
 
+        @PositiveOrZero(message = "제품 가격은 0 미만여서는 않됩니다.")
         private Long price;
 
+        @PositiveOrZero(message = "제품 재고는 0 미만여서는 않됩니다.")
         private Long stock;
 
         private ItemStatus status;
